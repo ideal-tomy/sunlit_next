833ce565d29a0db5046dcc5c41c933823a7b6af9
ideal-tomy <ryojitomii@gmail.com>
2025-07-11 22:40:41 +0900

    メール設定デプロイ用03

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -14,7 +14,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       }
 
       const { data, error } = await resend.emails.send({
-        from: 'SunlitWest Contact Form <SunlitWest <info@sunlit.fit>',
+        from: 'SunlitWest Contact Form <info@sunlit.fit>', // ←ここをこれに修正
         to: [toEmail],
         subject: `【HP問い合わせ】${subject}`,
         replyTo: email,
